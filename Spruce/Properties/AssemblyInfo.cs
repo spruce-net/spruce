f3bb199301f022ccec51db1c107020ef4607efa4
Jim Geurts <jim@biacreations.com>
2013-05-09 14:40:16 -0500

    Add nuget details

--- Spruce/Properties/AssemblyInfo.cs
+++ Spruce/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Spruce")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Opinionated set of extension methods representing a lightweight orm and migration framework for use with Dapper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bia Creations")]
 [assembly: AssemblyProduct("Spruce")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  Bia Creations")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
